eee42ef706341e7a6f87bdfb04842905fd57b288
Farrel Abennaya <114734387+farrelabennaya@users.noreply.github.com>
2023-06-20 00:51:08 +0700

    UTS

---
+++ src/bankSwing/InformasiSaldoModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankSwing;
+
+/**
+ *
+ * @author Abennaya
+ */
+public class InformasiSaldoModel {
+    String saldo="696969";
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/bankSwing/pinModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankSwing;
+
+/**
+ *
+ * @author Abennaya
+ */
+public class pinModel {
+    String pin="12345";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+}
